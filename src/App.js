b4c81976331ce52c12629b1431195afdb5a99541
marcelsguima <marcelsguima@gmail.com>
2022-11-23 14:53:43 -0300

    npm install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './Pages/Home';
+import TableProvider from './context/TableProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <TableProvider>
+      <HomePage />
+    </TableProvider>
   );
 }
 
